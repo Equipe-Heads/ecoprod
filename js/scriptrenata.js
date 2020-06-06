d2973d461dabfd37a0d189723083859c493b0d27
Renata <renatapca86@gmail.com>
2020-06-06 16:54:26 -0300

    UF e cidades

--- js/scriptrenata.js
+++ js/scriptrenata.js
@@ -1,5 +1,60 @@
 //botao Menu
 
+
+
+
+
+function populateUFs(){
+  const ufSelect = document.querySelector("select[name=uf]")
+  
+  fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+  .then( res => res.json() )
+  .then(states => {
+      for(const state of states){
+          ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+
+
+      }
+  } )
+}
+
+
+populateUFs()
+
+
+function getCities(event){
+  const citySelect = document.querySelector("select[name=city]")
+      console.log(event.target.value)
+      
+      const ufValue = event.target.value
+
+      const url=`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+
+        
+  fetch(url)
+  .then( res => res.json() )
+  .then(cities => {
+      for(const city of cities){
+          citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+      }
+
+  } )
+
+}
+
+
+
+
+
+
+
+document
+.querySelector("select[name=uf]")
+.addEventListener("change", getCities)
+
+
+
+
   // Or with jQuery
 
   $(document).ready(function(){
@@ -45,67 +100,3 @@ form.addEventListener('submit', () => {
 $(document).ready(function(){
   $('select').formSelect();
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function populateUFs(){
-  const ufSelect = document.querySelector("select[name=uf]")
-  
-  fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
-  .then( res => res.json() )
-  .then(states => {
-      for(const state of states){
-          ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
-
-
-      }
-  } )
-}
-
-
-populateUFs()
-
-
-function getCities(event){
-  const citySelect = document.querySelector("select[name=city]")
-      console.log(event.target.value)
-      
-      const ufValue = event.target.value
-
-      const url=`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
-
-        
-  fetch(url)
-  .then( res => res.json() )
-  .then(cities => {
-      for(const city of cities){
-          citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
-      }
-
-      citySelect.disabled = false
-  } )
-
-}
-
-
-
-
-
-
-
-document
-.querySelector("select[name=uf]")
-.addEventListener("change", getCities)
-
-
